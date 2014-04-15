cacf7065b026ee223a6d1f9fd62402f8024dd769
Thomas Åkesson <thomas.akesson@simonsoft.se>
2014-04-15 14:01:22 +0000

    Improved documentation of PublishService interface.

--- cms-publish-interface/trunk/src/main/java/se/simonsoft/cms/publish/PublishService.java
+++ cms-publish-interface/trunk/src/main/java/se/simonsoft/cms/publish/PublishService.java
@@ -15,14 +15,16 @@ public interface PublishService {
 
 	/**
 	 * Useful for generic user interfaces to list supported formats.
+	 * This is a forward looking method that may throw UnsupportedOperation at this time.
 	 * @return Non exhaustive list of formats that the service supports.
 	 */
 	public Set<PublishFormat> getPublishFormats();
 	
 	/**
 	 * 
-	 * @param format a 
-	 * @return
+	 * @param format as a string
+	 * @return the PublishFormat that specifies how the specific service provides that format. 
+	 * @throws PublishException if the format is not supported by the service
 	 */
 	public PublishFormat getPublishFormat(String format);
 
@@ -38,14 +40,27 @@ public interface PublishService {
 	PublishTicket requestPublish(PublishRequest request);
 	
 	
+	/**
+	 * @param ticket
+	 * @param request
+	 * @return true if the publish has completed, which does not necessarily mean it was successful.
+	 */
 	Boolean isCompleted(PublishTicket ticket, PublishRequest request);
 
-	// Is a stream the way we want to get the result? Also as a File?
-	void getResultStream(PublishTicket ticket, PublishRequest request, OutputStream outStream);
+	
+	/** Provide the result as a stream.
+	 * @param ticket
+	 * @param request
+	 * @param outStream where the result will be written
+	 * @throws PublishException if the publish was not successful
+	 */
+	void getResultStream(PublishTicket ticket, PublishRequest request, OutputStream outStream) throws PublishException;
 
 
 
-	/** investigate if we can provide the publish log this way. These have the concept: PE, Saxon, DITA-OT (lots of console, any readable?), Jenkins (console) */
+	/** Provide the log as a stream. Might be empty if the publish was a successful. 
+	 * These have the concept: PE, Saxon, DITA-OT (lots of console, any readable?), Jenkins (console) 
+	 */
 	void getLogStream(PublishTicket ticket, PublishRequest request, OutputStream outStream);
 
 	
