86d7d2a4cae27a987cdaac4418ead463933caff4
Jonathan Andersson <jonathan.andersson@simonsoft.se>
2017-11-15 13:14:04 +0100

    added method accept(PublishConfig, CmsItem)

--- cms-publish-rest/src/main/java/se/simonsoft/cms/publish/config/filter/PublishConfigFilter.java
+++ cms-publish-rest/src/main/java/se/simonsoft/cms/publish/config/filter/PublishConfigFilter.java
@@ -1,5 +1,10 @@
 package se.simonsoft.cms.publish.config.filter;
 
+import se.simonsoft.cms.item.CmsItem;
+import se.simonsoft.cms.publish.databinds.publish.config.PublishConfig;
+
 public interface PublishConfigFilter {
+	
+	boolean accept(PublishConfig config, CmsItem item);
 
 }
