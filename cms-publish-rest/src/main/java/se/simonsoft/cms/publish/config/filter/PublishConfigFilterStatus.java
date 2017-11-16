29d3ae94732c63bf9913b5d9452cb34268394024
Jonathan Andersson <jonathan.andersson@simonsoft.se>
2017-11-16 10:38:57 +0100

    We return true if list is null, otherwise the value of the items status must match a value in config.

--- cms-publish-rest/src/main/java/se/simonsoft/cms/publish/config/filter/PublishConfigFilterStatus.java
+++ cms-publish-rest/src/main/java/se/simonsoft/cms/publish/config/filter/PublishConfigFilterStatus.java
@@ -1,5 +1,7 @@
 package se.simonsoft.cms.publish.config.filter;
 
+import java.util.List;
+
 import se.simonsoft.cms.item.CmsItem;
 import se.simonsoft.cms.publish.databinds.publish.config.PublishConfig;
 
@@ -7,7 +9,17 @@ public class PublishConfigFilterStatus implements PublishConfigFilter {
 
 	@Override
 	public boolean accept(PublishConfig config, CmsItem item) {
-		return config.getStatusInclude().contains(item.getStatus());
+		
+		List<String> statusInclude = config.getStatusInclude();
+		if (statusInclude == null) {
+			return true;
+		} else if (statusInclude.isEmpty()) {
+			return false;
+		} else if (config.getStatusInclude().contains(item.getStatus())) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
