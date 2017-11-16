72ceb2084de397ac5b18dea18f719f15c974d746
Jonathan Andersson <jonathan.andersson@simonsoft.se>
2017-11-16 10:40:43 +0100

    Will check field embd_xml_a_type from item and match it with config.

--- cms-publish-rest/src/main/java/se/simonsoft/cms/publish/config/filter/PublishConfigFilterType.java
+++ cms-publish-rest/src/main/java/se/simonsoft/cms/publish/config/filter/PublishConfigFilterType.java
@@ -4,10 +4,13 @@ import se.simonsoft.cms.item.CmsItem;
 import se.simonsoft.cms.publish.databinds.publish.config.PublishConfig;
 
 public class PublishConfigFilterType implements PublishConfigFilter {
-
+	
+	private final String typeInclude = "embd_xml_a_type"; //TODO: Unsure if this is the correct name: type-include embd_xml_a_type
+	
 	@Override
 	public boolean accept(PublishConfig config, CmsItem item) {
-		return false;
+		String type = (String) item.getMeta().get(typeInclude);
+		return config.getOptions().getType().equals(type);
 	}
 
 }
