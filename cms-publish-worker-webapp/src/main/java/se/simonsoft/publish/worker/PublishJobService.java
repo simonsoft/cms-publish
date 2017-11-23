155b278bc2295fb04daf6892f413f9693a2c5f14
Sovsa <sovsa74@gmail.com>
2017-11-23 09:31:49 +0100

    Reworked the URL paths of the classes and methods in PublishJobService.java and PublishJobPage.java

--- cms-publish-worker-webapp/src/main/java/se/simonsoft/publish/worker/PublishJobService.java
+++ cms-publish-worker-webapp/src/main/java/se/simonsoft/publish/worker/PublishJobService.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2009-2017 Simonsoft Nordic AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package se.simonsoft.publish.worker;
 
 import java.io.BufferedReader;
@@ -12,6 +27,8 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
+import se.simonsoft.cms.item.CmsItemId;
+import se.simonsoft.cms.item.impl.CmsItemIdArg;
 import se.simonsoft.cms.publish.PublishException;
 import se.simonsoft.cms.publish.PublishFormat;
 import se.simonsoft.cms.publish.PublishTicket;
@@ -19,6 +36,7 @@ import se.simonsoft.cms.publish.abxpe.PublishServicePe;
 import se.simonsoft.cms.publish.databinds.publish.job.*;
 import se.simonsoft.cms.publish.impl.PublishRequestDefault;
 import se.simonsoft.cms.publish.PublishSource;
+import se.simonsoft.cms.publish.PublishSourceCmsItemId;
 
 public class PublishJobService {
 
@@ -43,6 +61,7 @@ public class PublishJobService {
 		request.addConfig("path", publishPath);
 		request = this.getConfigParams(request, job);
 		
+		System.out.println(job.getSource());
 		final String itemId = job.getSource();
 		PublishSource source = new PublishSource() {
 			
