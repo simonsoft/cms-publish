de8e26636603aae874eb411d0ca1dba70106df0f
Jonathan Andersson <jonathan.andersson@simonsoft.se>
2017-11-24 07:46:38 +0100

    Injection in Hk2 is not as straight forward as in guice. Could not find a way to inject a singleton, Creates instance of it and starts the listener from WorkerApplication. Still has my credentials in it and hardcoded activity arn.

--- cms-publish-worker-webapp/src/main/java/se/simonsoft/publish/worker/WorkerApplication.java
+++ cms-publish-worker-webapp/src/main/java/se/simonsoft/publish/worker/WorkerApplication.java
@@ -21,6 +21,8 @@ import javax.inject.Singleton;
 
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
@@ -36,6 +38,9 @@ import se.simonsoft.cms.publish.abxpe.PublishServicePe;
 import se.simonsoft.cms.publish.impl.PublishRequestDefault;
 
 public class WorkerApplication extends ResourceConfig {
+	
+	
+	private static final Logger logger = LoggerFactory.getLogger(WorkerApplication.class);
 
 	public WorkerApplication()  {
 		
@@ -46,34 +51,32 @@ public class WorkerApplication extends ResourceConfig {
             protected void configure() {
             	bind("injected").to(String.class).named("TEST");
             	bind(new PublishServicePe()).to(PublishServicePe.class);
-            	bind(new ObjectMapper()).to(ObjectMapper.class);
-            	bind(new PublishRequestDefault()).to(PublishRequestDefault.class);
-            	bind(PublishJobService.class); //TODO: This will probably not work.
+            	PublishServicePe publishServicePe = new PublishServicePe();
+            	PublishJobService publishJobService = new PublishJobService(publishServicePe);
+            	bind(publishJobService).to(PublishJobService.class);
             	
             	ClientConfiguration clientConfiguration = new ClientConfiguration();
         		clientConfiguration.setSocketTimeout((int)TimeUnit.SECONDS.toMillis(70));
             	AWSStepFunctions client = AWSStepFunctionsClientBuilder.standard()
         				.withRegion(Regions.EU_WEST_1)
-        				.withCredentials(getCredentials("AKIAIGW4IM6AQNOBQ2IA", "Nv2h5OVbfvMue5FQbfMoW+0JhftuNRp3OMXstGkC"))
+        				.withCredentials(getCredentials("AKIAIGW4IM6AQNOBQ2IA", "Nv2h5OVbfvMue5FQbfMoW+0JhftuNRp3OMXstGkC")) // Credentials, has to be injected.
         				.withClientConfiguration(clientConfiguration)
         				.build();
             	
             	bind(client).to(AWSStepFunctions.class);
-            	
-            	
-            	//Jackson
+//            	
+//            	//Jackson binding reader for future useage.
         		ObjectMapper mapper = new ObjectMapper();
-        		bind(mapper.reader()).to(ObjectReader.class);
-            	
-            	bind(AwsStepfunctionPublishWorker.class).to(Singleton.class); // this will probably not work.
+        		ObjectReader reader = mapper.reader();
+        		bind(reader).to(ObjectReader.class);
+//            	
+//        		//Not the easiest thing to inject a singleton. We create a instance of it here and let it start it self form its constructor.
+        		logger.debug("Starting publish worker...");
+        		new AwsStepfunctionPublishWorker(reader, client, "arn:aws:states:eu-west-1:148829428743:activity:cms-jandersson-abxpe", publishJobService);
+        		logger.debug("publish worker started.");
             }
         });
 		
-		
-		
-		//register(MyResource.class);
-	    //register(new MyProvider());
-	    //packages("se.simonsoft.publish.worker");
 	}
 	
 	private AWSCredentialsProvider getCredentials(final String id, final String secret) {
