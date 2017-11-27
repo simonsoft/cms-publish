c8e2764e28ae8dfe5aaa735cfaa598f8f8cb4336
Jonathan Andersson <jonathan.andersson@simonsoft.se>
2017-11-27 10:22:53 +0100

    Renamed export services.

--- cms-publish-config/src/main/java/se/simonsoft/cms/publish/export/PublishJobExportService.java
+++ cms-publish-config/src/main/java/se/simonsoft/cms/publish/export/PublishJobExportService.java
@@ -5,7 +5,7 @@ import java.io.OutputStream;
 
 import se.simonsoft.cms.publish.databinds.publish.job.PublishJobOptions;
 
-public interface PublishStorageExport {
+public interface PublishJobExportService {
 	
 	String exportJob(OutputStream os, PublishJobOptions jobOptions);
 	
