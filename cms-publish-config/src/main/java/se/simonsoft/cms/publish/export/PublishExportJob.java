d812d963cb3add72e8d8e4ee93fb8f8fbe0a45f3
Jonathan Andersson <jonathan.andersson@simonsoft.se>
2017-12-04 15:23:43 +0100

    Should be a zip. Writer can only handle single Items. Calling super (CmsExportJobZip) to get zip content and get resultStream

--- cms-publish-config/src/main/java/se/simonsoft/cms/publish/export/PublishExportJob.java
+++ cms-publish-config/src/main/java/se/simonsoft/cms/publish/export/PublishExportJob.java
@@ -18,10 +18,11 @@ package se.simonsoft.cms.publish.export;
 import java.io.OutputStream;
 
 import se.simonsoft.cms.item.export.CmsExportJob;
+import se.simonsoft.cms.item.export.CmsExportJobZip;
 import se.simonsoft.cms.item.export.CmsExportPrefix;
 import se.simonsoft.cms.publish.databinds.publish.job.PublishJobStorage;
 
-public class PublishExportJob extends CmsExportJob implements CmsExportJob.SingleItem {
+public class PublishExportJob extends CmsExportJobZip implements CmsExportJob.SingleItem {
 
 	public PublishExportJob(PublishJobStorage storage, String jobExtension) {
 		super(createJobPrefix(storage), createJobName(storage), jobExtension);
@@ -41,6 +42,6 @@ public class PublishExportJob extends CmsExportJob implements CmsExportJob.Singl
 
 	@Override
 	public void getResultStream(OutputStream out) {
-		getExportItems().get(0).getResultStream(out);
+		super.getResultStream(out);
 	}
 }
