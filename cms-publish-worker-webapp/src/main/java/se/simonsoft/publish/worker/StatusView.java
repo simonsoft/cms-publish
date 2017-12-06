b22717357cf2e5d7018b432605102ba11c89252f
Sovsa <sovsa74@gmail.com>
2017-12-06 13:46:38 +0100

    Moved all code from PublishJobPage to TestPage.java. TestPublishJobService.java now asserts the testdata against raw string data instead of de-serialized json object. Added page for Status(in progress) in the worker index. PublishJobService.getCompletedJob() now checks if the job is completed before doing anything else, if the job is not completed, a PublishException will be thrown

--- cms-publish-worker-webapp/src/main/java/se/simonsoft/publish/worker/StatusView.java
+++ cms-publish-worker-webapp/src/main/java/se/simonsoft/publish/worker/StatusView.java
@@ -15,10 +15,18 @@
  */
 package se.simonsoft.publish.worker;
 
+import java.io.StringWriter;
+import java.util.Properties;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
 
 @Path("status")
 public class StatusView {
@@ -26,9 +34,19 @@ public class StatusView {
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String getStatus() {
+		VelocityEngine engine = new VelocityEngine();
+		Properties p = new Properties();
+		p.setProperty(RuntimeConstants.RESOURCE_LOADER, "class");
+		p.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+		engine.init(p);
+
+		VelocityContext context = new VelocityContext();
 		
+		Template template = engine.getTemplate("se/simonsoft/publish/worker/templates/StatusTemplate.vm");
 		
-		return "Temporary text";
+		StringWriter wr = new StringWriter();
+		template.merge(context, wr);
 		
+		return wr.toString();
 	}
 }
