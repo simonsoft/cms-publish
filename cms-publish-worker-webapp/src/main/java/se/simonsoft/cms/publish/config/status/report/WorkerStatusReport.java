6293fe57ee1c7a63c1e2d0053b659d86d57a223d
Sovsa <sovsa74@gmail.com>
2017-12-12 11:46:34 +0100

    Moved WorkerStatusReport.java from se.simonsoft.cms.publish.config.status.report to se.simonsoft.cms.publish.config.status.report to avoid bad dependencies between pom files

--- cms-publish-worker-webapp/src/main/java/se/simonsoft/cms/publish/config/status/report/WorkerStatusReport.java
+++ cms-publish-worker-webapp/src/main/java/se/simonsoft/cms/publish/config/status/report/WorkerStatusReport.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2009-2017 Simonsoft Nordic AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package se.simonsoft.cms.publish.config.status.report;
 
 import java.text.DateFormat;
@@ -9,19 +24,19 @@ import java.util.List;
 public class WorkerStatusReport {
 	
 	private List<WorkerEvent> events = new ArrayList<WorkerEvent>();
+	public static final int MAX_LENGTH = 100;
 
 	public List<WorkerEvent> getWorkerEvents(){
 		return this.events;
 	}
-
+	
 	public void addWorkerEvent(WorkerEvent event) {
 		events.add(event);
-				if(events.size() > 100) {
+				if(events.size() > MAX_LENGTH) {
 					events.remove(1);
 				}
 	}
-
-
+	
 	public static class WorkerEvent {
 		private Date timeStamp;
 		private String action;
@@ -35,7 +50,7 @@ public class WorkerStatusReport {
 		}
 
 		public String getTimeStamp() {
-			DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+			DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 			return df.format(timeStamp);
 		}
 
