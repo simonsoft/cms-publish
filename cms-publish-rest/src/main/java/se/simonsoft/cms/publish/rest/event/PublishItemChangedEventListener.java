ea4d88f6fb45408a3a53404a3ff09a8c06eabf35
Thomas Åkesson <thomas.akesson@simonsoft.se>
2017-12-14 14:40:15 +0100

    Revert incorrect refactoring.

--- cms-publish-rest/src/main/java/se/simonsoft/cms/publish/rest/event/PublishItemChangedEventListener.java
+++ cms-publish-rest/src/main/java/se/simonsoft/cms/publish/rest/event/PublishItemChangedEventListener.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package se.simonsoft.cms.publish.rest.command;
+package se.simonsoft.cms.publish.rest.event;
 
 import java.io.IOException;
 import java.util.ArrayList;
