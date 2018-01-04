10935d4d194d89e7df2ebc03a82e45a4a55965bb
Jonathan Andersson <jonathan.andersson@simonsoft.se>
2018-01-04 08:20:37 +0100

    getReleaseForm works. And We are requesting files form aws. Still work in progress. Needs to null check values in velocity templates. Publication should be used as config name.

--- cms-publish-rest/src/main/java/se/simonsoft/cms/publish/rest/PublishResource.java
+++ cms-publish-rest/src/main/java/se/simonsoft/cms/publish/rest/PublishResource.java
@@ -15,9 +15,16 @@
  */
 package se.simonsoft.cms.publish.rest;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -25,8 +32,11 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.StreamingOutput;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
@@ -43,6 +53,8 @@ import se.simonsoft.cms.publish.config.databinds.config.PublishConfig;
 import se.simonsoft.cms.publish.config.databinds.profiling.PublishProfilingRecipe;
 import se.simonsoft.cms.publish.config.databinds.profiling.PublishProfilingSet;
 import se.simonsoft.cms.publish.config.item.CmsItemPublish;
+import se.simonsoft.cms.release.translation.CmsItemTranslation;
+import se.simonsoft.cms.release.translation.TranslationTracking;
 import se.simonsoft.cms.reporting.CmsItemLookupReporting;
 
 @Path("/publish4")
@@ -53,17 +65,21 @@ public class PublishResource {
 	private final PublishConfigurationDefault publishConfiguration;
 	private final PublishPackageZip repackageService;
 	private final ReposHtmlHelper htmlHelper;
+	private final Map<CmsRepository, TranslationTracking> trackingMap;
 
 	@Inject
 	public PublishResource(@Named("config:se.simonsoft.cms.hostname") String hostname,
 			Map<CmsRepository, CmsItemLookupReporting> lookup,
 			PublishConfigurationDefault publishConfiguration,
 			PublishPackageZip repackageService,
+			Map<CmsRepository, TranslationTracking> trackingMap,
 			ReposHtmlHelper htmlHelper) {
+		
 		this.hostname = hostname;
 		this.lookup = lookup;
 		this.publishConfiguration = publishConfiguration;
 		this.repackageService = repackageService;
+		this.trackingMap = trackingMap;
 		this.htmlHelper = htmlHelper;
 	}
 	
@@ -84,21 +100,27 @@ public class PublishResource {
 		CmsItem item = cmsItemLookupReporting.getItem(itemId);
 		CmsItemPublish itemPublish = new CmsItemPublish(item);
 		
+		logger.debug("Requesting profilingSet...");
 		PublishProfilingSet itemProfilingSet = publishConfiguration.getItemProfilingSet(itemPublish);
-		Map<String, PublishConfig> configuration = publishConfiguration.getConfigurationFiltered(itemPublish);
-		Map<String, PublishProfilingRecipe> itemProfiling = itemProfilingSet.getMap();
 		
-		VelocityEngine engine = new VelocityEngine();
-		Properties p = new Properties();
-		p.setProperty(RuntimeConstants.RESOURCE_LOADER, "class");
-		p.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
-		engine.init(p);
+		Map<String, PublishProfilingRecipe> itemProfilings = null;
+		if (itemProfilingSet == null) {
+			itemProfilings = new HashMap<String, PublishProfilingRecipe>(); //Setting it to empty hashMap to make velocity compile. Maybe we should make null check in template?
+			logger.debug("There is no profiles configured for item with id: {}", itemId);
+		} else {
+			itemProfilings = itemProfilingSet.getMap();
+			logger.debug("ItemId: {} has: {} configured profiles", itemId, itemProfilings.size());
+		}
+		
+		Map<String, PublishConfig> configuration = publishConfiguration.getConfigurationFiltered(itemPublish);
+		
+		VelocityEngine engine = getVelocityEngine(); //TODO: maybe we should inject velocity engine.
 
 		VelocityContext context = new VelocityContext();
 		Template template = engine.getTemplate("se/simonsoft/cms/publish/templates/batch-publish-template.vm");
 		
 		context.put("item", item);
-		context.put("itemProfiling", itemProfiling);
+		context.put("itemProfiling", itemProfilings);
 		context.put("configuration", configuration);
 		context.put("reposHeadTags", htmlHelper.getHeadTags(null));
 
@@ -112,10 +134,12 @@ public class PublishResource {
 	@Path("release/download")
 	@Produces("application/zip")
 	public Response getDownload(@QueryParam("item") CmsItemIdArg itemId,
-						@QueryParam("includemaster") boolean includeMaster,
+						@QueryParam("includerelease") boolean includeRelease,
 						@QueryParam("includetranslations") boolean includeTranslations,
 						@QueryParam("profiling") String[] profiling,
 						@QueryParam("publication") String publication) throws Exception {
+		
+		logger.debug("Download of item: {} requested with master: {}, translations: {} and profiles: {}", itemId, includeRelease, includeTranslations, Arrays.toString(profiling));
 		
 		if (itemId == null) {
 			throw new IllegalArgumentException("Field 'item': required");
@@ -127,8 +151,57 @@ public class PublishResource {
 			throw new IllegalArgumentException("Field 'item': revision is required");
 		}
 		
-		logger.debug("Download of item: {} requested, with master: {} and transaltions: {}", itemId, includeMaster, includeTranslations);
+		final Set<CmsItem> items = new HashSet<CmsItem>();
 		
-		return Response.ok("Succesfully called get release/download").build();
+		CmsItemLookupReporting lookupReporting = lookup.get(itemId.getRepository());
+		CmsItem masterItem = lookupReporting.getItem(itemId);
+		items.add(masterItem);
+		
+		Map<String, PublishConfig> configurationFiltered = publishConfiguration.getConfigurationFiltered(new CmsItemPublish(masterItem));
+		final PublishConfig simplePdfConfig = configurationFiltered.get("simple-pdf"); // trying with one known config to start with.
+		
+		List<CmsItemTranslation> translations = null;
+		if (includeTranslations) {
+			TranslationTracking translationTracking = trackingMap.get(itemId.getRepository());
+			translations = translationTracking.getTranslations(itemId); // Using deprecated method until TODO in translationTracking is resolved.
+			logger.debug("Found {} translations.", translations.size());
+			
+			for (CmsItemTranslation t: translations) {
+				CmsItem tItem = lookupReporting.getItem(t.getTranslation());
+				items.add(tItem);
+			}
+		}
+		
+	    StreamingOutput stream = new StreamingOutput() {
+            @Override
+            public void write(OutputStream os) throws IOException, WebApplicationException {
+            	repackageService.getZip(items, "simple-pdf", simplePdfConfig, null, os);
+            }
+        };
+		
+		ResponseBuilder responseBuilder = Response.ok(stream, MediaType.APPLICATION_OCTET_STREAM);
+		
+		return responseBuilder.build();
+	}
+	
+	private VelocityEngine getVelocityEngine() {
+		//TODO: Maybe this should be injected. Initializing a Velocity Engine requires configuration properties for logging that should be standard.
+		VelocityEngine engine = new VelocityEngine();
+		engine.setProperty("runtime.references.strict", true);
+		
+		Properties p = new Properties();
+		p.setProperty(RuntimeConstants.RESOURCE_LOADER, "class");
+		p.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+		p.put("runtime.log.logsystem.class", "org.apache.velocity.runtime.log.SimpleLog4JLogSystem");
+		p.put("runtime.log.logsystem.log4j.category", "velocity");
+		p.put("runtime.log.logsystem.log4j.logger", "velocity");
+		
+		try {
+			engine.init(p);
+		} catch (Exception e) {
+			throw new RuntimeException("Could not initilize Velocity engine with given properties.");
+		}
+		
+		return engine;
 	}
 }
