8677877c1b0058a9925bc3b4f8229cf21ffcf379
Thomas Åkesson <thomas.akesson@simonsoft.se>
2020-03-05 21:19:26 +0100

    #1277 Filter on pathext to avoid triggering publish for Translated Graphics. Not configurable at this time.

---
+++ cms-publish-rest/src/main/java/se/simonsoft/cms/publish/rest/config/filter/PublishConfigFilterPathext.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2009-2017 Simonsoft Nordic AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.simonsoft.cms.publish.rest.config.filter;
+
+import se.simonsoft.cms.item.CmsItem;
+import se.simonsoft.cms.item.structure.CmsItemClassification;
+import se.simonsoft.cms.item.structure.CmsItemClassificationXml;
+import se.simonsoft.cms.publish.config.databinds.config.PublishConfig;
+import se.simonsoft.cms.publish.config.item.CmsItemPublish;
+
+public class PublishConfigFilterPathext implements PublishConfigFilter {
+
+	private CmsItemClassification itemClassification = new CmsItemClassificationXml();
+	
+	@Override
+	public boolean accept(PublishConfig config, CmsItem item) {
+		
+		if (!(item instanceof CmsItemPublish)) {
+			return false;
+		}
+		
+		//CmsItemPublish itemPublish = (CmsItemPublish) item;
+		return itemClassification.isXml(item.getId());
+	}
+
+}
