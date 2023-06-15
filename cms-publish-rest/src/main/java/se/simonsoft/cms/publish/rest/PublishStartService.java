01e027bc48974653176f737d24c41830468dec14
Omid Manikhi <omidontop@gmail.com>
2023-06-15 10:24:29 +0200

    Partially implemented the PublishStartService

--- cms-publish-rest/src/main/java/se/simonsoft/cms/publish/rest/PublishStartService.java
+++ cms-publish-rest/src/main/java/se/simonsoft/cms/publish/rest/PublishStartService.java
@@ -15,90 +15,134 @@
  */
 package se.simonsoft.cms.publish.rest;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 import javax.inject.Inject;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.simonsoft.cms.item.CmsItem;
 import se.simonsoft.cms.item.CmsItemId;
+import se.simonsoft.cms.item.CmsRepository;
+import se.simonsoft.cms.item.command.CommandRuntimeException;
+import se.simonsoft.cms.item.command.ExternalCommandHandler;
 import se.simonsoft.cms.publish.config.PublishConfiguration;
 import se.simonsoft.cms.publish.config.PublishExecutor;
 import se.simonsoft.cms.publish.config.databinds.config.PublishConfig;
 import se.simonsoft.cms.publish.config.databinds.job.PublishJob;
+import se.simonsoft.cms.publish.config.databinds.job.PublishJobManifest;
 import se.simonsoft.cms.publish.config.databinds.profiling.PublishProfilingRecipe;
+import se.simonsoft.cms.publish.config.databinds.profiling.PublishProfilingSet;
 import se.simonsoft.cms.publish.config.item.CmsItemPublish;
+import se.simonsoft.cms.publish.rest.PublishJobFactory;
+import se.simonsoft.cms.publish.rest.PublishStartOptions;
+import se.simonsoft.cms.release.ProfilingSet;
+import se.simonsoft.cms.release.ReleaseProperties;
 import se.simonsoft.cms.release.translation.TranslationLocalesMapping;
 import se.simonsoft.cms.reporting.CmsItemLookupReporting;
 
 public class PublishStartService {
 
+	private final ObjectMapper objectMapper;
 	private final CmsItemLookupReporting lookupReporting;
 	private final PublishConfiguration publishConfiguration;
 	private final PublishExecutor publishExecutor;
 	private final PublishJobFactory jobFactory;
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(PublishStartService.class);
+
 	@Inject
 	public PublishStartService(
 			CmsItemLookupReporting lookupReporting,
 			PublishConfiguration publishConfiguration,
 			PublishExecutor publishExecutor,
-			PublishJobFactory jobFactory) {
-		
+			PublishJobFactory jobFactory,
+			ObjectMapper objectMapper) {
+
 		this.lookupReporting = lookupReporting;
 		this.publishConfiguration = publishConfiguration;
 		this.publishExecutor = publishExecutor;
 		this.jobFactory = jobFactory;
+		this.objectMapper = objectMapper;
 	}
-	
-	
+
+
 	public LinkedHashMap<String, String> doPublishStartItem(CmsItemId itemId, PublishStartOptions options) {
-		
+
 		PublishConfig config = getPublishConfiguration(itemId, options.getPublication());
 		PublishJob publishJob = getPublishJob(itemId, options, config);
-		
+
 		Set<String> uuids = publishExecutor.startPublishJobs(new LinkedHashSet<PublishJob>(Arrays.asList(publishJob)));
-		// TODO: Verify that one job was started, log the UUID.
-		// Note that this UUID is not the same as exectionuuid in PublishStartOptions. 
-		// The options.executionuuid is required before this UUID is known.
-		
+
+		// Verify that one job was started, log the UUID.
+		uuids.forEach(uuid -> logger.info("Publish job started: {}", uuid));
+		if (uuids.size() != 1) {
+			throw new CommandRuntimeException("StartPublishJobError", String.format("None or several publish jobs started: %d", uuids.size()));
+		}
+
 		LinkedHashMap<String, String> result = null; // TODO: Use PublishWebhookCommandHandler
-		
+
 		return result;
 	}
-	
-		
+
 	private PublishConfig getPublishConfiguration(CmsItemId itemId, String name) {
-		
+
 		Map<String, PublishConfig> configs = publishConfiguration.getConfiguration(itemId);
 		return configs.get(name);
 	}
 
 	private PublishJob getPublishJob(CmsItemId itemId, PublishStartOptions options, PublishConfig config) {
-		
+
 		CmsItem item = this.lookupReporting.getItem(itemId);
 		CmsItemPublish itemPublish = new CmsItemPublish(item);
 		TranslationLocalesMapping localesRfc = (TranslationLocalesMapping) this.publishConfiguration.getTranslationLocalesMapping(itemPublish);
 
 		PublishProfilingRecipe profilingRecipe = null;
 		// Use profilingname if set (get recipe from itemPublish)
-		// Use startprofiling if set (must not contain 'name' or 'logicalexpr')
-		// no profiling.
-		
+		if (itemPublish.hasProfiles() && options.getProfilingname() != null && options.getProfilingname().length() > 0) {
+			ObjectReader profilingReader = objectMapper.readerFor(ProfilingSet.class);
+			String profilingValue = item.getProperties().getString(ReleaseProperties.PROPNAME_PROFILING);
+			PublishProfilingSet profilingSet = ProfilingSet.getProfilingSet(profilingValue, profilingReader);
+			profilingRecipe = profilingSet.get(options.getProfilingname());
+		} else if (options.getStartprofiling() != null) { // Use startprofiling if set (must not contain 'name' or 'logicalexpr')
+			profilingRecipe = options.getStartprofiling();
+			if (profilingRecipe.getName() != null || profilingRecipe.getLogicalExpr() != null) {
+				throw new IllegalArgumentException("Dynamic profiling recipes must not contain 'name' or 'logicalexpr'");
+			}
+		} else {
+			// no profiling.
+			profilingRecipe = new PublishProfilingRecipe();
+		}
+
 		// Verify that the config is intended for profiling, if profilingRecipe != null.
-		
-		
+		if (profilingRecipe != null && !config.getProfilingInclude()) {
+			throw new IllegalArgumentException("Requested profiling is not properly configured");
+		}
+
+		if (options.getExecutionid() == null) {
+			throw new RuntimeException("No 'executionid' was assigned");
+		}
+
+		// TODO: Need specific scenariotest setting 'startinput' but not 'startprofiling'.
 		// Set profilingRecipe.name to executionid if any of the start* parameters are provided (even if profilingRecipe == null).
-		// Figure out how to manage profilingRecipe.name when no profiling is used (method validateFilter() throws exception when no filter parameters are set).
-		// Need specific scenariotest setting 'startinput' but not 'startprofiling'.
-		
+		if (options.getStartpathname() != null || options.getStartcustom().size() > 0) {
+			profilingRecipe.setAttribute("name", options.getExecutionid());
+		} else {
+			// TODO: Figure out how to manage profilingRecipe.name when no profiling is used (method validateFilter() throws exception when no filter parameters are set).
+		}
+
 		PublishJob job = jobFactory.getPublishJob(itemPublish, config, options.getPublication(), profilingRecipe, localesRfc, Optional.ofNullable(options.getStartpathname()), Optional.ofNullable(options.getStartcustom()));
+
+		PublishJobManifest manifest = job.getOptions().getManifest();
+		LinkedHashMap<String, String> custom = manifest.getCustom();
+		custom.putAll(options.getStartcustom());
+		manifest.setCustom(custom);
+		job.getOptions().setManifest(manifest);
+
 		return job;
 	}
-	
+
 }
